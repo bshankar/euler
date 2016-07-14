904c22049a3aac7d097cbbfdc68937d677351892
bshankar <ebhavanishankar@gmail.com>
2016-07-14 13:15:52 +0530

    solution to problem 493

--- 493.cpp
+++ 493.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <iomanip>
+#include "prime.h"
+#include <gmpxx.h>
 using namespace std;
 
 int main(int argc, const char *argv[]) {
+    double numerator, denominator;
+    denominator = combinations(70.0, 20.0);
+    numerator = 0;
 
-    double balls[] = {10, 10, 10, 10, 10, 10, 10}; 
-    double exp[20] = {}; // average color for each ball
-    double total = 70;
+    // Using inclusion exclusion
+    for (double n = 2; n <= 7; ++n) {
+        double factor = n*combinations(7.0, n);
+        double add = combinations(10*n, 20.0);
+        for (double k = n-1; k >= 2; --k)
+            add += pow((-1), (int) (n-k))*combinations(n, k)*combinations(10*k, 20.0);
+
+        numerator += factor*add;
+    }
+
+    cout << fixed << setprecision(9) << numerator/denominator << endl;
     
-    exp[0] = 1.0/7;
-
-    return 0;
+    // Easier method: probability of each color existing 
+    // in the chosen set of balls is 1 - C(60, 20)/C(70, 20)
+    cout << "More precisely: ";
+    mpq_class a = 60, b = 70, c = 20;
+    a = 7*(1 - combinations(a, c)/combinations(b, c));
+    a.canonicalize();
+    cout << a << endl;
 }
