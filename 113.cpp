7f26ccea02d7c7fada3e6f3ee4a7462225f7964c
bshankar <ebhavanishankar@gmail.com>
2016-07-30 16:04:18 +0530

    solved problem 113

--- 113.cpp
+++ 113.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
-#include <string>
+#include "prime.h"
+#include <iomanip>
 using namespace std;
-typedef unsigned long long ull;
 
-ull inc(string s, int len) {
-    return inc()
-}
+// maximum length of non-bouncy number
+#define N 100.0 
+#define B 10.0 // number base
 
 int main() {
-    unsigned long long ans = 0;
+    // After a long calculation
+    cout << setprecision(15) 
+         << combinations((N+B-1), B-1)*(N + 2*B)/B 
+            - (B*N + 2) << endl;
 }
