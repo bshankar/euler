331607522938268749b917981d5bde990f850c3c
bshankar <ebhavanishankar@gmail.com>
2016-08-16 16:07:42 +0530

    fast check if an integer is a perfect square

--- prime.h
+++ prime.h
@@ -292,3 +292,15 @@ vector<T> segmented_sieve(T limit,
     return primes_;
 }
 
+
+bool is_square(ul n) {
+    ul h = n & 0xF;  // h is the last hex "digit"
+    if (h > 9)
+        return 0;
+    // Use lazy evaluation to jump out of the if statement as soon as possible
+    if (h != 2 && h != 3 && h != 5 && h != 6 && h != 7 && h != 8) {
+        ul t = (ul) floor( sqrt((double) n) + 0.5 );
+        return t*t == n;
+    }
+    return 0;
+}
