a7789ee0e98d5bf6059a702a3e17fe9098b6c475
bshankar <ebhavanishankar@gmail.com>
2016-09-06 19:16:36 +0530

    Make solution to problem 12 faster using openmp

--- src/12.cpp
+++ src/12.cpp
@@ -7,14 +7,15 @@ using namespace std;
 template <class T>
 T cd(T n) {
     T d = 0;
-    for (T i = 1; i < sqrt(n); ++i) {
+    #pragma omp parallel for reduction(+: d)
+    for (T i = 1; i < (T) sqrt(n) + 1; ++i) {
         if (n % i == 0)
             d += 2;
     }
     return d;
 }
 
-int main(int argc, char** argv) {
+int main() {
     unsigned long n = 1, k = 1;
     while (cd(n) < 500) {
         ++k;
