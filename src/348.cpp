6dfc6fcd0f07a445affe876f42bf31c1893f3f9c
bshankar <ebhavanishankar@gmail.com>
2016-09-11 03:53:42 +0530

    solution to problem 348

---
+++ src/348.cpp
@@ -0,0 +1,37 @@
+#include "prime.h"
+#include "palindrome.h"
+#include <map>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    const ul N = 1000;
+    vector<ul> cubes;
+    map<string, us> count;
+
+    for (auto i = 0; i < N; ++i)
+        cubes.push_back(i*i*i);
+
+    int found_palindromes = 0;
+    string cp = "11"; // current palindrome
+    ul icp = stoul(cp),
+       ans = 0;
+
+    while (found_palindromes < 5) {
+        for (auto cube: cubes) {
+            if (cube >= icp)
+                break;
+            if (is_square(stoul(cp) - cube)) {
+                ++count[cp];
+
+                if (count[cp] == 4) {
+                    cout << "Found a palindrome " << cp << endl;
+                    ans += icp;
+                    ++found_palindromes;
+                }
+            }
+        }
+        cp = next_palindrome(cp);
+        icp = stoul(cp);
+    }
+    cout << ans << endl;
+}
