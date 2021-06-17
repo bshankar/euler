cc8c10d01027940c3aa18750fe488e315f1d8420
Bhavani Shankar <ebhavanishankar@gmail.com>
2021-06-17 20:19:40 +0530

    Add solution for 67

--- src/18.rkt
+++ src/18.rkt
@@ -1,16 +1,21 @@
 #lang racket
+(require memoize)
 
-(define (read-input)
+(define (read-input [path "../data/18.txt"])
   (map (λ (s) (map string->number s))
-    (map (λ (l) (string-split l " ")) (file->lines "../data/18.txt"))))
+    (map (λ (l) (string-split l " ")) (file->lines path))))
+(provide read-input)
 
 (define (get-weight grid i j)
   (list-ref (list-ref grid i) j))
 
-(define (shortest-path grid [i 0] [j 0])
+(define/memo (shortest-path grid i j)
   (if (= i (sub1 (length grid)))
       (get-weight grid i j)
-      (+ (get-weight grid i j) (max (shortest-path grid (add1 i) j)
-                                    (shortest-path grid (add1 i) (add1 j))))))
+      (+ (get-weight grid i j)
+         (max (shortest-path grid (add1 i) j)
+              (shortest-path grid (add1 i) (add1 j))))))
+(provide shortest-path)
 
-(shortest-path (read-input))
+(module* main #f
+  (time (shortest-path (read-input) 0 0)))
---
+++ src/67.rkt
@@ -0,0 +1,4 @@
+#lang racket
+(require "18.rkt")
+
+(time (shortest-path (read-input "../data/67.txt") 0 0))
