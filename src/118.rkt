f0dc8ae00a0dc99765b5c9d1f788287853bc6fa0
Bhavani Shankar <ebhavanishankar@gmail.com>
2021-06-26 01:13:45 +0530

    Optimize 118 to run in < 9s

--- src/118.rkt
+++ src/118.rkt
@@ -6,16 +6,19 @@
   (and (prime? lv) (or (empty? s) (> lv (last s)))))
 
 (define (prime-set-from? perm)
-  (let loop ([i 1] [n (ref perm 0)] [current #()] [acc 0])
+  (let loop ([perm (cdr perm)] [n (car perm)] [current #()] [acc 0])
     (cond
-      [(= i (length perm)) (if (valid-set-with? current n) (add1 acc) acc)]
+      [(empty? perm) (if (valid-set-with? current n) (add1 acc) acc)]
       [(valid-set-with? current n)
-       (+ (loop i 0 (conj current n) acc)
-          (loop (add1 i) (+ (ref perm i) (* 10 n)) current acc))]
-      [else (loop (add1 i) (+ (ref perm i) (* 10 n)) current acc)])))
+       (+ (loop perm 0 (conj current n) acc)
+          (loop (cdr perm) (+ (car perm) (* 10 n)) current acc))]
+      [else (loop (cdr perm) (+ (car perm) (* 10 n)) current acc)])))
+
+(define (valid-permutation? p)
+  (not (or (even? (last p)) (= (last p) 5))))
 
 (define (pandigital-prime-sets size)
-  (apply + (map prime-set-from?
-                (permutations (stream->list (range 1 (add1 size)))))))
+  (define perms (permutations (stream->list (range 1 (add1 size)))))
+  (apply + (map prime-set-from? (filter valid-permutation? perms))))
 
 (time (pandigital-prime-sets 9))
