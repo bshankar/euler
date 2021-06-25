1da361cd1f76a9bba7ee08cd16af32b221c7115c
Bhavani Shankar <ebhavanishankar@gmail.com>
2021-06-25 11:01:43 +0530

    Simplify 118 and make it faster

--- src/118.rkt
+++ src/118.rkt
@@ -1,52 +1,21 @@
 #lang racket
-(require (only-in math/number-theory prime? divides?)
-         "lib/number-theory.rkt" racket/trace)
+(require (only-in math/number-theory prime? factorial)
+         data/collection)
 
-(define (prepend-digit n d)
-  (+ n (* d (expt 10 (add1 (inexact->exact (floor (log n 10))))))))
+(define (valid-set-with? s lv)
+  (and (prime? lv) (or (empty? s) (> lv (last s)))))
 
-(define (prime-sets? ss)
-  (filter (λ (s) (andmap prime? s)) ss))
-
-(define (get perms p i)
-  (list-ref (vector-ref perms p) i))
-
-(define (valid-suffix? n)
-  (and (odd? n) (not (divides? 5 n))))
-
-(define (prime-append? ls d)
-  (for/list ([i (range (length ls))]
-             #:when (valid-suffix? (list-ref ls i)))
-    (define-values (x y) (split-at ls i))
-    (define yf (first y))
-    (define ys (rest y))
-    (if (or (even? yf) (divides? 5 yf)) '()
-        (append x (list (prepend-digit (first y) d)) (rest y)))))
-
-(define (with-digit ls d i)
-  (if (empty? ls)
-      (list (list d))
-      (for/fold ([res '()]) ([l ls])
-        (append res
-                (prime-append? l d)
-                (list (append l (list d)))))))
-
-(define (pandigital-prime-sets digits)
-  (define perms (list->vector (permutations (range 1 (add1 digits)))))
-  (let loop ([p 0] [i 0] [sets '()] [acc (set)])
+(define (prime-set-from? perm)
+  (let loop ([i 1] [n (ref perm 0)] [current #()] [acc 0])
     (cond
-      [(= p (vector-length perms)) (set-count acc)]
-      [(= i digits) (loop (add1 p) 0 '()
-                          (set-union
-                           acc
-                           (list->set (map list->set (prime-sets? sets)))))]
-      [else (loop p (add1 i) (with-digit sets (get perms p i) i) acc)])))
+      [(= i (length perm)) (if (valid-set-with? current n) (add1 acc) acc)]
+      [(valid-set-with? current n)
+       (+ (loop i 0 (conj current n) acc)
+          (loop (add1 i) (+ (ref perm i) (* 10 n)) current acc))]
+      [else (loop (add1 i) (+ (ref perm i) (* 10 n)) current acc)])))
 
-(module* main #f
-  (time (pandigital-prime-sets 9)))
+(define (pandigital-prime-sets size)
+  (apply + (map prime-set-from?
+                (permutations (stream->list (range 1 (add1 size)))))))
 
-(module+ test
-  (require rackunit)
-  (check-equal? (with-digit '() 1) (list '(1)))
-  (check-equal? (with-digit (list '(2 3)) 1)
-                (list '(21 3) '(2 31) '(2 3 1))))
+(time (pandigital-prime-sets 9))
