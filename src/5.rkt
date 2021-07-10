46ed097d7d6f76b2a4c2b9900087b5fee1cf941a
Bhavani Shankar <ebhavanishankar@gmail.com>
2021-07-10 12:39:56 +0530

    Add solution for 107

--- src/5.rkt
+++ src/5.rkt
@@ -5,7 +5,7 @@
   (inexact->exact (expt b (floor (log n b)))))
 
 (define (smallest-div-by-1-n n)
-  (apply * (map (λ(b) (highest-pow-less-than n b)) (sieve n))))
+  (apply * (map (λ (b) (highest-pow-less-than n b)) (sieve n))))
 
 (time (smallest-div-by-1-n 20))
 
